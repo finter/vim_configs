5c8eb5aecb9045c4c6c5790565a10fdfccc76266
Sergey Blinov <blinovsv@gmail.com>
2011-09-24 08:41:39 +0400

    Linkmaker script for Windows has added

---
+++ makelinks.bat
@@ -0,0 +1,8 @@
+@echo off
+del %USERPROFILE%\.vimrc
+del %USERPROFILE%\.gvimrc
+rmdir /S /Q %USERPROFILE%\vimfiles
+
+mklink /H %USERPROFILE%\.vimrc .vimrc
+mklink /H %USERPROFILE%\.gvimrc .gvimrc
+mklink /D %USERPROFILE%\vimfiles .vim
